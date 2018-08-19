ef691feb969bef6b7cf36a81395f8c2b45c0794f
sauravdas90 <saurav.d336@gmail.com>
2018-08-20 01:43:35 +0530

    initial commit for second app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { UserListComponent } from './users/user-list/user-list.component';
+import { UserItemComponent } from './users/user-item/user-item.component';
+import { UserCreateComponent } from './users/user-create/user-create.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserListComponent,
+    UserItemComponent,
+    UserCreateComponent
   ],
   imports: [
     BrowserModule
